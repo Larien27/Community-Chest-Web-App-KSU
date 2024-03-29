b5c57f86d7beed22da7e60075331c326df11c337
Lucie Bekeova <lbekeova@ksu.edu>
2024-03-29 20:37:01 +0000

    Display new request form in location detail

--- src/endpoints/show-location.js
+++ src/endpoints/show-location.js
@@ -4,7 +4,9 @@ const db = require('../database');
 function showRequest(req, res) {
   const id = parseInt(req.params.id, 10);
   var box = db.prepare("SELECT * FROM boxes WHERE id = ?").get(id);
-  var html = templates["location.html"]({box: box});
+  var requests = db.prepare("SELECT * FROM requests WHERE box_id = ?").all(id);
+  var requestHtml = templates["new-request.html"]();
+  var html = templates["location.html"]({box: box, requests: requestHtml});
   res.setHeader("Content-Type", "text/html");
   res.setHeader("Content-Length", html.length);
   res.end(html);
