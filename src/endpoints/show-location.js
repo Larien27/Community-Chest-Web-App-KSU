64098043a89ec76fb943e77983f0de5abf28c158
Lucie Bekeova <lbekeova@ksu.edu>
2024-03-29 20:45:08 +0000

    Added box id in new request form

--- src/endpoints/show-location.js
+++ src/endpoints/show-location.js
@@ -5,7 +5,7 @@ function showRequest(req, res) {
   const id = parseInt(req.params.id, 10);
   var box = db.prepare("SELECT * FROM boxes WHERE id = ?").get(id);
   var requests = db.prepare("SELECT * FROM requests WHERE box_id = ?").all(id);
-  var requestHtml = templates["new-request.html"]();
+  var requestHtml = templates["new-request.html"]({boxId: box.id});
   var html = templates["location.html"]({box: box, requests: requestHtml});
   res.setHeader("Content-Type", "text/html");
   res.setHeader("Content-Length", html.length);
