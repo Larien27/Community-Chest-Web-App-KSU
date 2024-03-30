8b40aa9b0fa289fbec4a2b7f8850008ee647b9a1
Lucie Bekeova <lbekeova@ksu.edu>
2024-03-30 04:50:34 +0000

    Working but detailed location not aesthetically pleasing

--- src/app.js
+++ src/app.js
@@ -2,15 +2,14 @@ const express = require('express');
 const boxLocations = require('./endpoints/box-locations');
 const newRequest = require('./endpoints/new-request');
 const createRequest = require('./endpoints/create-request');
-const loadBody = require('./middleware/load-body');
+const parseBody = require('./middleware/parse-body');
 const showLocation = require('./endpoints/show-location');
 
 var app = express();
 
 app.get(['/', '/box-locations'], boxLocations);
 app.get('/box-locations/:id', showLocation);
-app.post('/box-locations/:id/requests', loadBody, createRequest);
-//app.post('/box-locations', loadBody, createRequest);
+app.post('/box-locations/:id/requests', parseBody, createRequest);
 
 app.use(express.static('static'));
 
