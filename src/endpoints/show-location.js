9f8cb997429dc8f5e6c852d20bd47c24c1de95e7
Lucie Bekeova <lbekeova@ksu.edu>
2024-04-01 02:22:50 +0000

    Milestone 4

--- src/endpoints/show-location.js
+++ src/endpoints/show-location.js
@@ -7,19 +7,6 @@ function showLocation(req, res) {
   var requests = db.prepare("SELECT * FROM requests WHERE box_id = ?").all(id);
   var form = templates["new-request.html"]({boxId: id});
   
-  /* DEBUG */
-  const currentTime = new Date();
-  console.log(currentTime);
-  const path = require('path');
-  console.log(path.basename(__filename));
-  
-  console.log(box);
-  console.log("Lat: " + box.lat);
-  console.log("Lng: " + box.lng);
-  console.log("Name: " + box.name);
-  console.log("ID: " + box.id);
-  /* END OF DEBUG */
-  
   var location = templates["location.html"]({box: box, requests: requests, requestForm: form});
   var html = templates["layout.html"]({title: box.name, boxes: location});
   res.setHeader("Content-Type", "text/html");
