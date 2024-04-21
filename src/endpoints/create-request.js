bca314b307dd4e76ae7229f8103312bee24b18b6
Lucie Bekeova <lbekeova@ksu.edu>
2024-04-21 16:49:30 +0000

    Request form only for logged in users - Milestone 5

--- src/endpoints/create-request.js
+++ src/endpoints/create-request.js
@@ -4,13 +4,14 @@ const serveError = require('../serve-error');
 
 function createRequest(req, res) {
   var boxId = req.body.boxId;
+  var userId = req.session.user.id;
   var request = req.body.request;
   var fulfilled = 0;
   request = sanitizeHTML(request);
 
   if(!request) return serveError(req, res, 422, "Empty request encountered");
 
-  var info = db.prepare("INSERT INTO REQUESTS (box_id, request, fulfilled) VALUES (?, ?, ?)").run(boxId, request, fulfilled);
+  var info = db.prepare("INSERT INTO REQUESTS (box_id, request, fulfilled, user_id) VALUES (?, ?, ?, ?)").run(boxId, request, fulfilled, userId);
   if(info.changes != 1) return serveError(req, res, 500, `Unable to insert ${boxId}, ${request}, ${fulfilled} into requests`);
   res.writeHead(302, {"Location": `/box-locations/${boxId}`}).end(); 
 }
--- src/endpoints/show-location.js
+++ src/endpoints/show-location.js
@@ -5,9 +5,9 @@ function showLocation(req, res) {
   const id = parseInt(req.params.id, 10);
   var box = db.prepare("SELECT * FROM boxes WHERE id = ?").get(id);
   var requests = db.prepare("SELECT * FROM requests WHERE box_id = ?").all(id);
-  var form = templates["new-request.html"]({boxId: id});
+  var requestForm = templates["new-request.html"]({boxId: id});
   
-  var location = templates["location.html"]({box: box, requests: requests, requestForm: form, user: req.session.user});
+  var location = templates["location.html"]({box: box, requests: requests, requestForm: requestForm, user: req.session.user});
   var html = templates["layout.html"]({title: box.name, boxes: location, user: req.session.user});
   res.setHeader("Content-Type", "text/html");
   res.setHeader("Content-Length", html.length);
